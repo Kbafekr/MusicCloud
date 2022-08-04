d9af9a732beac7c7c12c8adadd36dcca6ed5425f
Keanen <KeanenBafekr@me.com>
2022-08-04 14:12:52 -0400

    seeder file  validation issue with playlistsongs

--- backend/db/seeders/20220804084935-playlistsongs.js
+++ backend/db/seeders/20220804084935-playlistsongs.js
@@ -14,34 +14,34 @@ module.exports = {
 
     await queryInterface.bulkInsert('playlistsongs', [
     {
-      playlistId:1,
-      songId:1
+      playlistId: 1,
+      songId: 1
     },
     {
-      playlistId:1,
-      songId:2
+      playlistId: 1,
+      songId: 2
     },
     {
-      playlistId:1,
-      songId:3
+      playlistId: 1,
+      songId: 3
     },
     {
-      playlistId:2,
-      songId:1
+      playlistId: 2,
+      songId: 1
     },
     {
-      playlistId:2,
-      songId:3
+      playlistId: 2,
+      songId: 3
     },
     {
-      playlistId:3,
-      songId:2
+      playlistId: 3,
+      songId: 2
     },
     {
-      playlistId:3,
-      songId:3
+      playlistId: 3,
+      songId: 3
     }
-  ], {})
+  ], {});
   },
 
   async down (queryInterface, Sequelize) {

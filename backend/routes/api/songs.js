0b9aa86d7f83cc0e04ca69d00da1137c113eb0d6
Keanenb <keanenbafekr@me.com>
2022-08-08 03:05:22 -0400

    query for createdAt

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -89,7 +89,19 @@ const SongValidation = [
 
       if (title) where.title = title
 
-      if (createdAt) where.createdAt =  createdAt 
+      if (createdAt)
+       { const date = createdAt.slice(0, 10)
+        const time = createdAt.slice(11, createdAt.length)
+
+        let datetime = date;
+
+        if (time) {
+        datetime = date.concat(`T${time}`)
+        }
+
+        console.log("this is time" + datetime)
+          {where.createdAt = { [Op.startsWith]: datetime } }
+       }
 
       page = parseInt(page)
       size = parseInt(size)

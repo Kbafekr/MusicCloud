359eb9015de77f43192170eaeae3743eebb3de55
Keanenb <keanenbafekr@me.com>
2022-08-18 22:41:19 -0400

    initial setup for frontend redux with react

---
+++ frontend/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './index.css';
+
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+import configureStore from './store';
+
+
+const store = configureStore();
+
+/* variable to access your store and expose it to the window.
+ It should not be exposed in production, be sure this is only set in development.
+ */
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+/* Root React functional component that returns the App component wrapped in Redux's Provider
+and React Router DOM's BrowserRouter provider components.
+*/
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+/* all ReactDOM.render function
+passing in the Root component and the HTML element with the id of "root".
+*/
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
---
+++ frontend/src/store/index.js
@@ -0,0 +1,29 @@
+// frontend/src/store/index.js
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+
+//rootreducer
+const rootReducer = combineReducers({
+    //add reducer functions here
+});
+
+/* Initialize an enhancer variable that will be set to different store enhancers
+depending on if the Node environment is in development or production. */
+let enhancer;
+
+if (process.env.NODE_ENV === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = require('redux-logger').default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+//configure store function to attach redux to react application
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+  };
+
+  export default configureStore;

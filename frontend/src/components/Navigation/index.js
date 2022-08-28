dbc8be0c2f0e434a3cde870ee4815ab8640276f6
Keanenb <keanenbafekr@me.com>
2022-08-27 22:57:53 -0400

    css

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,70 +1,70 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 //logged out imports
-import LoginFormModal from '../LoginFormModal';
-import SignUpModal from '../SignUpModal';
-import LoginAsDemo from '../LoginDemoUser';
+import LoginFormModal from "../LoginFormModal";
+import SignUpModal from "../SignUpModal";
+import LoginAsDemo from "../LoginDemoUser";
 //sign in imports for navbar
-import ProfileButton from './ProfileButton';
-import SongButton from './SongButton';
-import HomeButton from './HomeButton';
-import LibraryButton from './LibraryButton';
-import AlbumButton from './AlbumButton';
+import ProfileButton from "./ProfileButton";
+import SongButton from "./SongButton";
+import HomeButton from "./HomeButton";
+import LibraryButton from "./LibraryButton";
+import AlbumButton from "./AlbumButton";
 
-import './Navigation.css';
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-  const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
   let sessionLinks;
   let memberLinks;
 
   if (sessionUser) {
-   memberLinks = (
+    memberLinks = (
       <>
-      <div className='memberLinks'>
-      <HomeButton user={sessionUser} />
-      <LibraryButton user={sessionUser} />
-      <SongButton user={sessionUser} />
-      <AlbumButton user={sessionUser} />
-      </div>
+        <div className="memberLinks">
+          <HomeButton user={sessionUser} />
+          <LibraryButton user={sessionUser} />
+          <SongButton user={sessionUser} />
+          <AlbumButton user={sessionUser} />
+        </div>
       </>
-    )
+    );
 
     sessionLinks = (
       <>
-      <div className='SignInSide'>
-      <ProfileButton user={sessionUser} />
-      </div>
+        <div className="SignInSide">
+          <ProfileButton user={sessionUser} />
+        </div>
       </>
     );
   } else {
     sessionLinks = (
       <>
-      <div className='SignupSide'>
-        <LoginFormModal />
-        <SignUpModal />
-        <LoginAsDemo />
-      </div>
+        <div className="SignupSide">
+          <LoginFormModal />
+          <SignUpModal />
+          <LoginAsDemo />
+        </div>
       </>
     );
   }
 
   return (
-  <div className='NavBarContainer'>
-    {/* <div className='navBarLeftSide'> */}
-      <NavLink exact to="/">
-      <img className='HomeLogo' src={require('./MusicCloudLogo.png')} alt='MusicCloud Logo'/>
-      </NavLink>
-      <div className='membershipContainer'>
-      {memberLinks}
+    <div className="NavBarContainer">
+      <div className="LogoSide">
+        <NavLink exact to="/">
+          <img
+            className="LogoNav"
+            src={require("./MusicCloudLogo.png")}
+            alt="MusicCloud Logo"
+          />
+        </NavLink>
       </div>
-    {/* </div> */}
-    <div className='navBarRightSide'>
-        {isLoaded && sessionLinks}
+      <div className="navBarLeftSide">{memberLinks}</div>
+      <div className="navBarRightSide">{isLoaded && sessionLinks}</div>
     </div>
-  </div>
   );
 }
 

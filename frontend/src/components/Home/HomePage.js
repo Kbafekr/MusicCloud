3d157beb6b53d7b2f7af3a54d350df12195e1fd1
Keanenb <keanenbafekr@me.com>
2022-08-28 10:41:24 -0400

    homepage

--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -15,9 +15,13 @@ export function HomePage() {
     useEffect(() => {
         if (imageNumber < ImagesArray.length) {
            const ImageTransition =  setInterval(() => {
+            duration: 1,
+            
+
             //check to see if previous number is greater than images array length, if not then
                 setImageNumber((previousImageNumber) => (previousImageNumber + 1) % ImagesArray.length)
             }, 2000)
+
             return () => clearInterval(ImageTransition)
         }
         else {

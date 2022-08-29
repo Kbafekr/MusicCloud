23f368871894819e50e516fc434fbbbfece3b655
Keanenb <keanenbafekr@me.com>
2022-08-29 03:26:54 -0400

    comments

--- frontend/src/components/Home/HomePage.js
+++ frontend/src/components/Home/HomePage.js
@@ -129,7 +129,7 @@ export function HomePage() {
 
           <div
             className={
-              titleFiltered.length ? "SearchResult-container" : "Hidden"
+              titleFiltered.length ? "SearchResult-container" : "HiddenResult"
             }
           >
             {/* search return map */}

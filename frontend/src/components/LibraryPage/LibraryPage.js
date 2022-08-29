6209be61e96f6990d68f512e07c44faaae627516
Keanenb <keanenbafekr@me.com>
2022-08-29 05:41:50 -0400

    if not signed in

--- frontend/src/components/LibraryPage/LibraryPage.js
+++ frontend/src/components/LibraryPage/LibraryPage.js
@@ -5,6 +5,13 @@ import {useSelector} from 'react-redux'
 import {ImagesArray} from '../../images/Images'
 import LibraryGif from '../../images/LibraryGif.gif'
 
+
+import LoginAsDemo from "../LoginDemoUser";
+import Whomp from "../../images/Whomp.webp";
+import "../UnknownPage/PageNotFound.css";
+import "../Navigation/Navigation.css";
+
+
 export function LibraryPage() {
     const UserSignedIn = useSelector(state => state.session.user)
 
@@ -26,24 +33,22 @@ export function LibraryPage() {
     }
     else {
         return (
-            <div className='homePage'>
-
-                <div className='headers'>
-                 <h1>Insert search bar for tracks and artists & signin button that redirects to songs page</h1>
-
-                 <h1>Here's whats trending in the MusicCloud Community</h1>
-                 <h2>add 'explore trending playlists' button</h2>
-
-                 <h1>Thanks for listening. Now join in!</h1>
-                 <h2>Save tracks, follow artists and build playlists. All for free.</h2>
-
-                 <h3> create account button</h3>
-                 <h4>already have an account? + sign in button</h4>
-
-                 <h5>footer (include github link, modal for language selection, other bs)</h5>
+            <div className="errorPage">
+              <h1>Whomp Whomp!</h1>
+              <div className="Whomps">
+                <img className="whomp1" src={Whomp} alt="Whomp1" />
+                <img className="whomp2" src={Whomp} alt="Whomp2" />
+              </div>
+              <div className="headers">
+                <h2>Looks like you're an unauthorized user</h2>
+                <div className="linkerror">
+                  <h3>
+                    Sign in as a Demo User Below:
+                    <LoginAsDemo />
+                  </h3>
                 </div>
+              </div>
             </div>
-
-        )
-    }
+          );
+        }
 }

88c6b21f1eda9ab3494542716b1b38c946af48dc
Keanenb <keanenbafekr@me.com>
2022-11-20 18:42:01 -0500

    Update 20220803211643-genres.js

--- backend/db/seeders/20220803211643-genres.js
+++ backend/db/seeders/20220803211643-genres.js
@@ -24,403 +24,49 @@ module.exports = {
       {
       userId: 1,
       songId: 1,
-      body: 'Hip-Hop'
+      genre: 'Hip-Hop'
       },
       {
         userId: 1,
         songId: 1,
-        body: 'Rap'
+        genre: 'Rap'
       },
       {
         userId: 2,
         songId: 2,
-        body: 'Hip-Hop'
+        genre: 'Hip-Hop'
       },
       {
         userId: 3,
         songId: 3,
-        body: 'Rap'
+        genre: 'Rap'
       },
       {
         userId: 1,
         songId: 4,
-        body: 'Pop'
+        genre: 'Pop'
       },
       {
       userId: 3,
       songId: 5,
-      body: 'Pop'
+      genre: 'Pop'
       },
       {
         userId: 1,
         songId: 6,
-        body: 'Rock'
+        genre: 'Rock'
       },
       {
         userId: 2,
         songId: 7,
-        body: 'Jazz'
+        genre: 'Jazz'
       },
       {
         userId: 2,
         songId: 8,
-        body: 'Swing'
+        genre: 'Swing'
       },
-      {
-        userId: 3,
-        songId: 3,
-        body: 'opinion'
-      },
-      {
-      userId: 1,
-      songId: 1,
-      body: 'bark'
-      },
-      {
-        userId: 2,
-        songId: 1,
-        body: 'this is a comment'
-      },
-      {
-        userId: 1,
-        songId: 1,
-        body: 'first'
-      },
-      {
-        userId: 1,
-        songId: 1,
-        body: 'new comment'
-      },
-      {
-        userId: 3,
-        songId: 1,
-        body: 'opinion'
-      },
-      {
-      userId: 1,
-      songId: 2,
-      body: 'bark'
-      },
-      {
-        userId: 2,
-        songId: 2,
-        body: 'this is a comment'
-      },
-      {
-        userId: 1,
-        songId: 2,
-        body: 'first'
-      },
-      {
-        userId: 1,
-        songId: 3,
-        body: 'new comment'
-      },
-      {
-        userId: 3,
-        songId: 3,
-        body: 'opinion'
-      },
-      {
-        userId: 1,
-        songId: 4,
-        body: 'bark'
-        },
-        {
-          userId: 2,
-          songId: 4,
-          body: 'this is a comment'
-        },
-        {
-          userId: 1,
-          songId: 5,
-          body: 'first'
-        },
-        {
-          userId: 1,
-          songId: 5,
-          body: 'new comment'
-        },
-        {
-          userId: 3,
-          songId: 6,
-          body: 'opinion'
-        },
-        {
-        userId: 1,
-        songId: 6,
-        body: 'bark'
-        },
-        {
-          userId: 2,
-          songId: 7,
-          body: 'this is a comment'
-        },
-        {
-          userId: 1,
-          songId: 8,
-          body: 'first'
-        },
-        {
-          userId: 1,
-          songId: 9,
-          body: 'new comment'
-        },
-        {
-          userId: 3,
-          songId: 10,
-          body: 'opinion'
-        },
-        {
-      userId: 1,
-      songId: 1,
-      body: 'bark'
-      },
-      {
-        userId: 2,
-        songId: 1,
-        body: 'this is a comment'
-      },
-      {
-        userId: 1,
-        songId: 1,
-        body: 'first'
-      },
-      {
-        userId: 1,
-        songId: 1,
-        body: 'new comment'
-      },
-      {
-        userId: 3,
-        songId: 1,
-        body: 'opinion'
-      },
-      {
-      userId: 1,
-      songId: 2,
-      body: 'bark'
-      },
-      {
-        userId: 2,
-        songId: 2,
-        body: 'this is a comment'
-      },
-      {
-        userId: 1,
-        songId: 2,
-        body: 'first'
-      },
-      {
-        userId: 1,
-        songId: 3,
-        body: 'new comment'
-      },
-      {
-        userId: 3,
-        songId: 3,
-        body: 'opinion'
-      },
-      {
-        userId: 1,
-        songId: 11,
-        body: 'bark'
-        },
-        {
-          userId: 2,
-          songId: 12,
-          body: 'this is a comment'
-        },
-        {
-          userId: 1,
-          songId: 13,
-          body: 'first'
-        },
-        {
-          userId: 1,
-          songId: 14,
-          body: 'new comment'
-        },
-        {
-          userId: 3,
-          songId: 15,
-          body: 'opinion'
-        },
-        {
-        userId: 1,
-        songId: 16,
-        body: 'bark'
-        },
-        {
-          userId: 2,
-          songId: 17,
-          body: 'this is a comment'
-        },
-        {
-          userId: 1,
-          songId: 18,
-          body: 'first'
-        },
-        {
-          userId: 1,
-          songId: 19,
-          body: 'new comment'
-        },
-        {
-          userId: 3,
-          songId: 20,
-          body: 'opinion'
-        },
-        {
-          userId: 1,
-          songId: 21,
-          body: 'bark'
-          },
-          {
-            userId: 2,
-            songId: 22,
-            body: 'this is a comment'
-          },
-          {
-            userId: 1,
-            songId: 23,
-            body: 'first'
-          },
-          {
-            userId: 1,
-            songId: 24,
-            body: 'new comment'
-          },
-          {
-            userId: 3,
-            songId: 25,
-            body: 'opinion'
-          },
-          {
-          userId: 1,
-          songId: 26,
-          body: 'bark'
-          },
-          {
-            userId: 2,
-            songId: 27,
-            body: 'this is a comment'
-          },
-          {
-            userId: 1,
-            songId: 28,
-            body: 'first'
-          },
-          {
-            userId: 1,
-            songId: 29,
-            body: 'new comment'
-          },
-          {
-            userId: 3,
-            songId: 30,
-            body: 'opinion'
-          },
-          {
-            userId: 1,
-            songId: 31,
-            body: 'bark'
-            },
-            {
-              userId: 2,
-              songId: 32,
-              body: 'this is a comment'
-            },
-            {
-              userId: 1,
-              songId: 33,
-              body: 'first'
-            },
-            {
-              userId: 1,
-              songId: 34,
-              body: 'new comment'
-            },
-            {
-              userId: 3,
-              songId: 35,
-              body: 'opinion'
-            },
-            {
-            userId: 1,
-            songId: 36,
-            body: 'bark'
-            },
-            {
-              userId: 2,
-              songId: 37,
-              body: 'this is a comment'
-            },
-            {
-              userId: 1,
-              songId: 38,
-              body: 'first'
-            },
-            {
-              userId: 1,
-              songId: 39,
-              body: 'new comment'
-            },
-            {
-              userId: 3,
-              songId: 40,
-              body: 'opinion'
-            },
-            {
-              userId: 1,
-              songId: 41,
-              body: 'bark'
-              },
-              {
-                userId: 2,
-                songId: 42,
-                body: 'this is a comment'
-              },
-              {
-                userId: 1,
-                songId: 43,
-                body: 'first'
-              },
-              {
-                userId: 1,
-                songId: 44,
-                body: 'new comment'
-              },
-              {
-                userId: 3,
-                songId: 45,
-                body: 'opinion'
-              },
-              {
-              userId: 1,
-              songId: 46,
-              body: 'bark'
-              },
-              {
-                userId: 2,
-                songId: 47,
-                body: 'this is a comment'
-              },
-              {
-                userId: 1,
-                songId: 48,
-                body: 'first'
-              },
-              {
-                userId: 1,
-                songId: 49,
-                body: 'new comment'
-              },
-              {
-                userId: 3,
-                songId: 50,
-                body: 'opinion'
-              },
+
 
     ], {});
   },

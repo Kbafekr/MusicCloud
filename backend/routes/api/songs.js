bc4193fea46e050b46a55d136c5df45e396926aa
Keanen <KeanenBafekr@me.com>
2022-11-23 14:10:38 -0500

    Update songs.js

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -3,26 +3,26 @@
 // AWS Boilerplate
 import AWS from 'aws-sdk'
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-})
+// const s3 = new AWS.S3({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+// })
 
-const filename = 'the-file-name'
-const fileContent = fs.readFileSync(fileName)
+// const filename = 'the-file-name'
+// const fileContent = fs.readFileSync(fileName)
 
-const params = {
-  Bucket: process.env.AWS_BUCKET_NAME,
-  Key: `${filename}.jpg`,
-  Body: fileContent
-}
+// const params = {
+//   Bucket: process.env.AWS_BUCKET_NAME,
+//   Key: `${filename}.jpg`,
+//   Body: fileContent
+// }
 
-s3.upload(params, (err, data) => {
-  if (err) {
-    reject(err)
-  }
-  resolve(data.Location)
-})
+// s3.upload(params, (err, data) => {
+//   if (err) {
+//     reject(err)
+//   }
+//   resolve(data.Location)
+// })
 
 const express = require('express');
 
